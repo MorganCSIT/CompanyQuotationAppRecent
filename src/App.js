6e7ab89b8361dd472e2578af9d62a71f1078f34f
Morgan <morgankieffer@gmail.com>
2022-03-22 11:48:27 +0700

    Fresh repo commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState, useRef, useEffect } from 'react';
+import { Container, Row, Col, Button, Form, Navbar, Nav } from 'react-bootstrap';
+import { useLocalStorage } from 'react-use';
+import Quotation from './components/Quotation';
+import ProductManagement from './components/ProductManagement';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+	return (
+		<Router>
+			<Navbar bg="dark" variant="dark">
+				<Container>
+					<Navbar.Brand href="#home">VMS Company</Navbar.Brand>
+					<Nav className="me-auto">
+						<Nav.Link href="/react-quotation">Home</Nav.Link>
+						<Nav.Link href="#features">Features</Nav.Link>
+						<Nav.Link href="#pricing">Pricing</Nav.Link>
+						<Nav.Link href="/react-quotation/product-management">Product</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+			<Routes>
+				<Route path="/react-quotation/product-management" element={<ProductManagement />} />
+
+				<Route path="/react-quotation" path="/react-quotation" element={<Quotation />} />
+			</Routes>
+		</Router>
+	);
+}
+
+export default App;
---
+++ src/components/ProductManagement.js
@@ -0,0 +1,144 @@
+import React, { useEffect, useState, useRef } from "react";
+import {
+  Container,
+  Table,
+  Button,
+  Modal,
+  Form,
+  Row,
+  Col,
+} from "react-bootstrap";
+import style from "../mystyle.module.css";
+import { FaTrashAlt, FaPenAlt } from "react-icons/fa";
+
+export default function ProductManagement() {
+  const API_URL = process.env.REACT_APP_API_URL;
+
+  const [products, setProducts] = useState([]);
+  const [productRows, setProductRows] = useState([]);
+  const [modeAdd, setModeAdd] = useState(false);
+
+  const refCode = useRef()
+  const refName = useRef()
+  const refPrice = useRef()
+ 
+
+  useEffect(() => {
+    fetch(`${API_URL}/products`)
+      .then((res) => res.json())
+      .then((data) => {
+        const rows = data.map((e) => {
+          return (
+            <tr>
+              <td>
+                <FaPenAlt onClickCapture={handleShow} />
+                &nbsp; &nbsp;
+                <FaTrashAlt onClick={() => {}} />
+              </td>
+              <td>{e.code}</td>
+              <td>{e.name}</td>
+              <td>{e.price}</td>
+            </tr>
+          );
+        });
+
+        setProducts(data);
+        setProductRows(rows);
+      });
+  }, []);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    setShow(false);
+    setModeAdd(false);
+  };
+
+  const handleShow = () => setShow(true);
+
+  const handleShowAdd = () => {
+    setShow(true);
+    setModeAdd(true);
+  };
+
+  const handleFormAction = () => {
+    if(modeAdd) {
+      const newProduct = {
+        code: refCode.current.value,
+        name: refName.current.value,
+        price: refPrice.current.value
+      };
+        console.log(newProduct);
+  
+    } else {
+      //update product
+    }
+    }
+
+
+  return (
+    <>
+      <Container>
+        API_URL: {API_URL}
+        <h1>Product Management</h1>
+        <Button variant="outline-success" onClick={handleShowAdd}>
+          Add
+        </Button>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th style={{ width: "70px" }}>&nbsp;</th>
+              <th className={style.textCenter}>Code</th>
+              <th className={style.textCenter}>Name</th>
+              <th className={style.textCenter}>Price/Unit</th>
+            </tr>
+          </thead>
+          <tbody>{productRows}</tbody>
+        </Table>
+      </Container>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {modeAdd ? "Add new product" : "Update Product"}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Row>
+              <Col>Code</Col>
+              <Col>
+                <input type="text" ref={refCode} />
+              </Col>
+            </Row>
+            <Row>
+              <Col>Name</Col>
+              <Col>
+                <input type="text"  ref={refName} />
+              </Col>
+            </Row>
+            <Row>
+              <Col>Price</Col>
+              <Col>
+                <input type="number" ref={refPrice} />
+              </Col>
+            </Row>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleFormAction}>
+              {modeAdd ? 'Add' : 'Update'}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
